311a778ad2fbcb36ca538a72cc0b245a82ed5a2f
Josorod <romanshemenda@gmail.com>
2024-09-30 19:25:51 +0300

    added simple recommender

--- WebAPI/Models/Book.cs
+++ WebAPI/Models/Book.cs
@@ -2,14 +2,11 @@
 {
     public class Book
     {
-        public int Id { get; set; }
-        public string Book_Name { get; set; }
-        public string Author_Name { get; set; }
-        public string Genre { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
-        public string PdfUrl { get; set; }
-        public int Quantity { get; set; }
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public List<string> Genres { get; set; }
+        public List<string> Tags { get; set; }
     }
+
 }
